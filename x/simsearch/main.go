29e418e56364d94ae98b0e40d89f2a9677e27957
Martin Czygan <martin.czygan@gmail.com>
2025-10-04 16:42:49 +0200

    update notes

--- x/simsearch/main.go
+++ x/simsearch/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"context"
 	"encoding/json"
 	"flag"
@@ -14,11 +15,13 @@ import (
 )
 
 var (
-	ollamaHost = flag.String("s", os.Getenv("OLLAMA_HOST"), "ollama host")
-	modelName  = flag.String("m", "nomic-embed-text", "model name")
-	filename   = flag.String("f", "data/pg1661.json", "data file to read text snippets from")
-	store      = flag.String("w", "embed.json", "file to save embeddings to")
-	limit      = flag.Int("l", 100, "limit requests")
+	ollamaHost         = flag.String("s", os.Getenv("OLLAMA_HOST"), "ollama host")
+	modelName          = flag.String("m", "nomic-embed-text", "model name")
+	filename           = flag.String("f", "data/pg1661.json", "data file to read text snippets from")
+	store              = flag.String("w", "embed.json", "file to save embeddings to")
+	limit              = flag.Int("l", 10, "limit requests")
+	doCreateEmbeddings = flag.Bool("e", false, "create embeddings for data")
+	doRandomTriplet    = flag.Bool("3", false, "random three")
 
 	// (1) take three snippets and compare / which one is closer to the other?
 	// (2) enter some text and find similar snippets
@@ -38,69 +41,84 @@ type Set struct {
 
 func main() {
 	flag.Parse()
-	f, err := os.Open(*filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-	br := bufio.NewReader(f)
-	set := Set{}
-	for {
-		b, err := br.ReadBytes('\n')
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		var chunk Chunk
-		if err := json.Unmarshal(b, &chunk); err != nil {
-			log.Fatal(err)
-		}
-		set.Chunks = append(set.Chunks, chunk)
-	}
-	if _, err := os.Stat(*store); err == nil {
-		log.Printf("output %s exists, nothing to do", *store)
-		return
-	}
-	client, err := api.ClientFromEnvironment()
-	if err != nil {
-		log.Fatal(err)
-	}
-	version, err := client.Version(context.TODO())
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("ollama client %v", version)
-	log.Printf("generating embeddings...")
-	// TODO: batching
-	for i := range len(set.Chunks) {
-		if i == *limit {
-			break
-		}
-		req := &api.EmbedRequest{
-			Model: *modelName,
-			Input: set.Chunks[i].Text,
-		}
-		resp, err := client.Embed(context.TODO(), req)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if len(resp.Embeddings) < 1 {
-			log.Println("warn: empty reply")
-			continue
-		}
-		set.Chunks[i].Embedding = resp.Embeddings[0]
-	}
-	for i, chunk := range set.Chunks {
-		if i == *limit {
-			break
-		}
-		b, err := json.Marshal(chunk)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(string(b))
-	}
 
+	switch {
+	case *doCreateEmbeddings:
+		f, err := os.Open(*filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		br := bufio.NewReader(f)
+		set := Set{}
+		for {
+			b, err := br.ReadBytes('\n')
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatal(err)
+			}
+			var chunk Chunk
+			if err := json.Unmarshal(b, &chunk); err != nil {
+				log.Fatal(err)
+			}
+			set.Chunks = append(set.Chunks, chunk)
+		}
+		if _, err := os.Stat(*store); err == nil {
+			log.Printf("output %s exists, nothing to do", *store)
+			return
+		}
+		client, err := api.ClientFromEnvironment()
+		if err != nil {
+			log.Fatal(err)
+		}
+		version, err := client.Version(context.TODO())
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("ollama client %v", version)
+		log.Printf("generating embeddings...")
+		// TODO: batching
+		for i := range len(set.Chunks) {
+			if i%50 == 0 {
+				log.Printf("@%v", i)
+			}
+			if *limit > 0 && i == *limit {
+				break
+			}
+			req := &api.EmbedRequest{
+				Model: *modelName,
+				Input: set.Chunks[i].Text,
+			}
+			resp, err := client.Embed(context.TODO(), req)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if len(resp.Embeddings) < 1 {
+				log.Println("warn: empty reply")
+				continue
+			}
+			set.Chunks[i].Embedding = resp.Embeddings[0]
+		}
+		// save updated chunks
+		var buf bytes.Buffer
+		var enc = json.NewEncoder(&buf)
+		for i, chunk := range set.Chunks {
+			if *limit > 0 && i == *limit {
+				break
+			}
+			err := enc.Encode(chunk)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		if err := os.WriteFile(*store, buf.Bytes(), 0644); err != nil {
+			fmt.Println(buf.String())
+		}
+	case *doRandomTriplet:
+		if _, err := os.Stat(*store); os.IsNotExist(err) {
+			log.Fatal("create embedding first")
+		}
+	}
 }
